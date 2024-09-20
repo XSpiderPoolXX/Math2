074171b3ff2d6414c59cceb11bacbd554acb9174
Xavier Taylor <tayloxav002@gmail.com>
2024-09-20 08:30:11 -0400

    Initial commit.

---
+++ Main.py
@@ -0,0 +1,8 @@
+miles= 5
+hours=60
+mph= miles/hours
+minutes= 45
+hours = minutes/60
+print("how many miles did you run?", miles)
+print("How many minutes did it take you?",minutes)
+print("speed in mph:", mph)
